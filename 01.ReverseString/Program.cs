758727efe539205f0e780aeabb285e74144c6eb8
Filip Mavrodiev <Filip.Mavrodiev@tek-experts.com>
2018-10-11 18:32:52 +0300

    'lab'

---
+++ 01.ReverseString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToReverse = Console.ReadLine();
+
+            char[] charArray = stringToReverse.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            Console.WriteLine(new string(charArray));
+        }
+    }
+}
---
+++ 04.Palindromes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Palindromes
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine()
+                .Split(new char[] {' ', ',', '.', '!', '?'}, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in input)
+            {
+                string reverse = new string(word.Reverse().ToArray());
+                if (reverse == word)
+                {
+                    Console.Write($"{word}, ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
